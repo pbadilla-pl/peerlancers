461dead9964873faefc3493986f176988a0f4052
lskaxiefam <zeekillingmachine98@gmail.com>
2022-06-06 19:42:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const init = function() {
   let items = document.querySelectorAll("li");
   for (let i = 0; i < items.length; i++) {
     items[i].style.minHeight = "100vh";
-    // items[i].style.background = '#' + Math.floor(Math.random()*16777215).toString(16); //randomColor({ luminosity: "light" });
   }
-
-  //cssScrollSnapPolyfill();
 };
 document.onreadystatechange = function () {
   if (document.readyState === "interactive") {
